4a2bbe1e32cdbbfc353426c14d0f6346899793c1
Martin Folke <martin@Martins-MacBook-Pro.local>
2021-07-05 20:12:18 +0100

    herku deploy

--- meetingapp/settings.py
+++ meetingapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from developer_settings import DJANGO_SECRET_KEY, DATABASE
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,13 +20,32 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    from developer_settings import DJANGO_SECRET_KEY, DATABASE
+
+    SECRET_KEY = DJANGO_SECRET_KEY
+else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+    DATABASE = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'meetingapp',
+            'USER': 'martin',
+            'PASSWORD': 'Letmein86',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
+
+
+
+
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
